f839323e4da80379d2b1a5c81dda60b58b6ff0eb
harshit <harshit.raghav_cs19@gla.ac.in>
2020-08-28 18:02:56 +0530

    add name field ; change the name of the fields(secondNameOfStudent->middleNameOfStudent,thirdNameOfStudent->lastNameOfStudent);add and and changed methods to set the name of Student; modified equals, hashCode and toString method;cleanup commit   in Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -10,76 +10,93 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class Student {
+    private String nameOfStudent;
     private String firstNameOfStudent;
-    private String secondNameOfStudent;
-    private String thirdNameOfStudent;
+    private String middleNameOfStudent;
+    private String lastNameOfStudent;
     private long universityRollNumber;
     private int numberOfBooks;
     private Book[] books;
 
     public String getNameOfStudent() {
-        return firstNameOfStudent + secondNameOfStudent + thirdNameOfStudent;
+        return firstNameOfStudent + middleNameOfStudent + lastNameOfStudent;
+    }
+
+    public void setNameOfStudent(String firstNameOfStudent) {
+        this.firstNameOfStudent = firstNameOfStudent;
+        nameOfStudent = this.firstNameOfStudent;
     }
 
     public long getUniversityRollNumber() {
         return universityRollNumber;
     }
 
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+
     public int getNumberOfBooks() {
         return numberOfBooks;
-    }
-
-    public Book[] getBooks() {
-        return books.clone();
-    }
-
-    public void setNameOfStudent(String firstNameOfStudent, String secondNameOfStudent,
-                                 String thirdNameOfStudent) {
-        this.firstNameOfStudent = firstNameOfStudent;
-        this.secondNameOfStudent = secondNameOfStudent;
-        this.thirdNameOfStudent = thirdNameOfStudent;
     }
 
     public void setNumberOfBooks(int numberOfBooks) {
         this.numberOfBooks = numberOfBooks;
     }
 
-    public void setUniversityRollNumber(long universityRollNumber) {
-        this.universityRollNumber = universityRollNumber;
+    public Book[] getBooks() {
+        return books.clone();
     }
 
     public void setBooks(Book[] books) {
         this.books = books;
     }
 
+    public void setNameOfStudent(String firstNameOfStudent, String secondNameOfStudent) {
+        this.firstNameOfStudent = firstNameOfStudent;
+        this.middleNameOfStudent = secondNameOfStudent;
+        nameOfStudent = this.firstNameOfStudent + this.middleNameOfStudent;
+    }
+
+    public void setNameOfStudent(String firstNameOfStudent, String secondNameOfStudent,
+                                 String thirdNameOfStudent) {
+        this.firstNameOfStudent = firstNameOfStudent;
+        this.middleNameOfStudent = secondNameOfStudent;
+        this.lastNameOfStudent = thirdNameOfStudent;
+        nameOfStudent = this.firstNameOfStudent + this.middleNameOfStudent
+                + this.lastNameOfStudent;
+    }
+
     @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        Student student = (Student) object;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
         return getUniversityRollNumber() == student.getUniversityRollNumber() &&
                 getNumberOfBooks() == student.getNumberOfBooks() &&
+                Objects.equals(getNameOfStudent(), student.getNameOfStudent()) &&
                 Objects.equals(firstNameOfStudent, student.firstNameOfStudent) &&
-                Objects.equals(secondNameOfStudent, student.secondNameOfStudent) &&
-                Objects.equals(thirdNameOfStudent, student.thirdNameOfStudent) &&
+                Objects.equals(middleNameOfStudent, student.middleNameOfStudent) &&
+                Objects.equals(lastNameOfStudent, student.lastNameOfStudent) &&
                 Arrays.equals(getBooks(), student.getBooks());
     }
 
     @Override
     public int hashCode() {
-        int result = Objects.hash(firstNameOfStudent, secondNameOfStudent, thirdNameOfStudent, getUniversityRollNumber(), getNumberOfBooks());
+        int result = Objects.hash(getNameOfStudent(), firstNameOfStudent, middleNameOfStudent, lastNameOfStudent, getUniversityRollNumber(), getNumberOfBooks());
         result = 31 * result + Arrays.hashCode(getBooks());
         return result;
     }
 
     @Override
     public String toString() {
-        return "Student : \n" +
-                "Name of student : " + firstNameOfStudent + secondNameOfStudent +
-                thirdNameOfStudent +
-                "\n UniversityRollNumber :" + universityRollNumber +
-                "\n numberOfBooks : " + numberOfBooks +
-                "\n books : " + Arrays.toString(books) +
+        return "Student{" +
+                "nameOfStudent='" + nameOfStudent + '\'' +
+                ", firstNameOfStudent='" + firstNameOfStudent + '\'' +
+                ", middleNameOfStudent='" + middleNameOfStudent + '\'' +
+                ", lastNameOfStudent='" + lastNameOfStudent + '\'' +
+                ", universityRollNumber=" + universityRollNumber +
+                ", numberOfBooks=" + numberOfBooks +
+                ", books=" + Arrays.toString(books) +
                 '}';
     }
 }
