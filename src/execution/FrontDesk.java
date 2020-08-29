8c2fa4a05327474108e361ca8a769e87dbc41853
harshit <harshit.raghav_cs19@gla.ac.in>
2020-08-29 21:06:30 +0530

    display the menu message in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,5 +1,5 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+ *  User: Harshit Raghav (HarshitRaghav55)
  *  Date: 21/08/20
  *  Time: 3:50 PM
  *  File Name : FrontDesk.java
@@ -39,6 +39,12 @@ public class FrontDesk {
                 + student.getNumberOfBooks());
         System.out.print("Names of the books issued by the student :\n ");
         student.getNameOfBooks();
+        System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-\n" +
+                "How may I help you today?\n" +
+                "1. Issue a new book for me.\n" +
+                "2. Return a previously issues book for me.\n" +
+                "3. Show me all my issues books.\n" +
+                "4. Exit.\n");
 
     }
 }
